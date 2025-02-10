11aad1b50e773af7cdb3c3f7f2d048cca227e420
Conor Lewis <lewisconorbusiness@outlook.com>
2025-02-10 14:37:07 +0000

    V1

--- src/index.ts
+++ src/index.ts
@@ -11,8 +11,41 @@
  * Learn more at https://developers.cloudflare.com/workers/
  */
 
+import { hash } from "crypto";
+
+const ConvertToHex = (digest : ArrayBuffer) => [...new Uint8Array(digest)].map(v => v.toString(16).padStart(2, '0')).join('');
+
 export default {
 	async fetch(request, env, ctx): Promise<Response> {
-		return new Response('Hello World!');
+		const requestURL = new URL(request.url);
+		const pathArray = requestURL.pathname.split('/').splice(1);
+
+		switch(pathArray[0]) {
+			case 'p': {
+				const url = requestURL.searchParams.get("url");
+				if(url == null) { return new Response("No URL Provided", { status: 400 }); }
+
+				const encodedURL = new TextEncoder().encode(url);
+				const hash512 = ConvertToHex(await crypto.subtle.digest({ name: "SHA-512" }, encodedURL));
+				const hash256 = ConvertToHex(await crypto.subtle.digest({ name: "SHA-256" }, encodedURL));
+
+				const hash = `${hash256.slice(0, 4)}${hash512.slice(hash512.length - 5, hash512.length - 1)}`;
+
+				if((await env.kv.get(hash)) == null) { await env.kv.put(hash, url) }
+
+				const shortURL = `${process.env.NODE_ENV === "development" ? `http://localhost:8787` : `https://urls.conorlewis.com`}/s/${hash}`
+
+				return new Response(`Accepted.\nShort URL: ${shortURL}\nOriginal URL: ${url}`, { status: 201 });
+			}
+			case 's':
+				const hash = pathArray[1];
+				if(hash == null) { return new Response("No UUID Provided", { status: 400 }); }
+
+				const result = await env.kv.get(hash);
+				if(result == null) { return new Response("Not Found", { status: 404 }); }
+
+				return Response.redirect(result, 302);
+			default: return new Response("Not Found", { status: 404 });
+		}
 	},
 } satisfies ExportedHandler<Env>;
